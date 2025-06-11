65e574d9ebb047057f8f6fb80caea4e3dabc1a79
dffdwi <daffadwi2003@gmail.com>
2025-06-12 05:53:11 +0700

    add new badge,mission,shop entity

---
+++ src/modules/mission/mission.entity.ts
@@ -0,0 +1,52 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+import { Badge } from '../badge/badge.entity';
+
+@Table({
+  tableName: 'missions',
+  timestamps: true,
+})
+export class Mission extends Model<Mission> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  missionId!: string;
+
+  @Column(DataType.STRING)
+  title!: string;
+
+  @Column(DataType.TEXT)
+  description!: string;
+
+  @Column(DataType.ENUM('once', 'daily', 'weekly'))
+  type!: string;
+
+  @Column(DataType.INTEGER)
+  target!: number;
+
+  @Default(0)
+  @Column(DataType.INTEGER)
+  rewardXp!: number;
+
+  @Default(0)
+  @Column(DataType.INTEGER)
+  rewardCredits!: number;
+
+  @ForeignKey(() => Badge)
+  @Column({
+    type: DataType.UUID,
+    allowNull: true,
+  })
+  rewardBadgeId?: string;
+
+  @BelongsTo(() => Badge)
+  rewardBadge?: Badge;
+}
