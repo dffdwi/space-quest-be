859a1cf87f22c750931383eeb44f1953943a58fb
dffdwi <daffadwi2003@gmail.com>
2025-06-13 15:37:09 +0700

    add shop purchase endpoint and service

--- src/modules/shop/shop.module.ts
+++ src/modules/shop/shop.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ShopItem } from './shop_item.entity';
 import { PlayerInventory } from './player_inventory.entity';
+import { ShopService } from './shop.service';
+import { ShopController } from './shop.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([ShopItem, PlayerInventory])],
+  imports: [
+    SequelizeModule.forFeature([ShopItem, PlayerInventory]),
+    forwardRef(() => UserModule),
+  ],
+  providers: [ShopService],
+  controllers: [ShopController],
   exports: [SequelizeModule],
 })
 export class ShopModule {}
